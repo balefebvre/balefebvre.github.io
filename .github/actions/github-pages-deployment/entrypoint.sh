588e2adeed867927427a6d8fc9482d580b99becd
balefebvre <baptiste.lefebvre@ens.fr>
2020-04-12 20:37:42 +0200

    Test

--- .github/actions/github-pages-deployment/entrypoint.sh
+++ .github/actions/github-pages-deployment/entrypoint.sh
@@ -4,3 +4,11 @@ echo "REPO: $1"
 echo "TOKEN: $2"  # TODO redacted or keep this secret?
 echo "REPO: $REPO"  # TODO redacted or keep this secret?
 echo "TOKEN: $TOKEN"  # TODO redacted or keep this secret?
+
+# TODO login!
+
+# TODO setup!
+
+# TODO prepare!
+
+# TODO deploy!

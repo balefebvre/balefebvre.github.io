b146336bfae797fccd69bbb89b77925891750a40
balefebvre <baptiste.lefebvre@ens.fr>
2020-04-12 19:39:23 +0200

    Test fix

--- .github/actions/github-pages-deployment/entrypoint.sh
+++ .github/actions/github-pages-deployment/entrypoint.sh
@@ -7,4 +7,4 @@ TOKEN=$2
 echo "TOKEN: $TOKEN"  # TODO redacted or keep this secret?
 
 echo "INPUTS_TOKEN: $INPUTS_TOKEN"  # TODO redacted or keep this secret?
-echo "SECRETS_TOKEN: $SECRETS_TOKEN"  # TODO redacted or keep this secret?
+# echo "SECRETS_TOKEN: $SECRETS_TOKEN"  # TODO redacted or keep this secret?

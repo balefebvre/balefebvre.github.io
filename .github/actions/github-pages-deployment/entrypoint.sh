f4f1c1a82b584be2c04ede0c1fef2df1a3ad6cd4
balefebvre <baptiste.lefebvre@ens.fr>
2020-04-12 20:50:38 +0200

    WIP

--- .github/actions/github-pages-deployment/entrypoint.sh
+++ .github/actions/github-pages-deployment/entrypoint.sh
@@ -1,11 +1,18 @@
 #!/bin/sh -l
 
-echo "REPO: $1"
-echo "TOKEN: $2"  # TODO redacted or keep this secret?
-echo "REPO: $REPO"  # TODO redacted or keep this secret?
-echo "TOKEN: $TOKEN"  # TODO redacted or keep this secret?
+# echo "REPO: $1"  # TODO remove?
+# echo "TOKEN: $2"  # TODO remove?
+# echo "REPO: $REPO"  # TODO remove?
+# echo "TOKEN: $TOKEN"  # TODO remove?
 
 # TODO login!
+if $TOKEN != ""; then
+  echo "TODO login token."  # TODO correct!
+  exit 1  # TODO remove?
+else
+  echo "TODO setup deploy key."  # TODO correct!
+  exit 1  # TODO remove?
+fi
 
 # TODO setup!
 

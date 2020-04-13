7658d98253835b67e8f341c597f8c9703442e6e4
balefebvre <baptiste.lefebvre@ens.fr>
2020-04-13 11:53:59 +0200

    WIP

--- .github/actions/github-pages-deployment/entrypoint.sh
+++ .github/actions/github-pages-deployment/entrypoint.sh
@@ -1,20 +1,41 @@
 #!/bin/sh -l
 
-# echo "REPO: $1"  # TODO remove?
-# echo "TOKEN: $2"  # TODO remove?
-# echo "REPO: $REPO"  # TODO remove?
-# echo "TOKEN: $TOKEN"  # TODO remove?
+# Display default environment variables
+echo "HOME: $HOME"  # TODO remove!
+echo "GIHTUB_WORKFLOW: $GITHUB_WORKFLOW"  # TODO remove!
+echo "GITHUB_RUN_ID: $GITHUB_RUN_ID"  # TODO remove!
+echo "GITHUB_RUN_NUMBER: $GITHUB_RUN_NUMBER"  # TODO remove!
+echo "GITHUB_ACTION: $GITHUB_ACTION"  # TODO remove!
+echo "GITHUB_ACTIONS: $GITHUB_ACTIONS"  # TODO remove!
+echo "GITHUB_ACTOR: $GITHUB_ACTOR"  # TODO remove!
+echo "GITHUB_REPOSITORY: $GITHUB_REPOSITORY"  # TODO remove!
+echo "GITHUB_EVENT_NAME: $GITHUB_EVENT_NAME"  # TODO remove!
+echo "GITHUB_WORKSPACE: $GITHUB_WORKSPACE"  # TODO remove!
+echo "GITHUB_SHA: $GITHUB_SHA"  # TODO remove!
+echo "GITHUB_REF: $GITHUB_REF"  # TODO remove!
+echo "GITHUB_HEAD_REF: $GITHUB_HEAD_REF"  # TODO remove!
+echo "GITHUB_BASE_REF: $GITHUB_BASE_REF"  # TODO remove!
+echo "GITHUB_TOKEN: $GITHUB_TOKEN"  # TODO remove!
+
+# Display custom environment variables
+echo "REPO: $REPO"  # TODO remove!
+echo "TOKEN: $TOKEN"  # TODO remove!
+echo "LOCAL_DIRECTORY: $LOCAL_DIRECTORY"  # TODO remove!
+echo "NAME: $NAME"  # TODO remove!
+echo "EMAIL: $EMAIL"  # TODO remove!
+
+# Miscellaneous
+echo "PWD: $(pwd)"  # TODO remove!
+echo "LS: $(ls)"  # TODO remove!
+
 
 # Login
 echo "# Login"
-echo "TOKEN: $TOKEN"  # TODO remove!
-if [ -n "$TOKEN" ]  # i.e. test if TOKEN is not empty
+if [ -n "$TOKEN" ]  # i.e. test if not empty
 then
   echo "## Login token"
-  # TODO recover user login.
-  echo "GITHUB_ACTOR: $GITHUB_ACTOR"  # TODO remove?
-  # TODO display info.
-  echo "Authenticated as $GITHUB_ACTOR"  # TODO remove?
+  USER_LOGIN="$GITHUB_ACTOR"  # TODO correct?
+  echo "Authenticated as $USER_LOGIN"
   # TODO check if token scope is sufficient?
 else
   echo "## Setup deploy key"
@@ -23,19 +44,25 @@ fi
 
 # Setup
 echo "# Setup"
-echo "HOME: $HOME"  # TODO remove!
-echo "GITHUB_WORKSPACE: $GITHUB_WORKSPACE"  # TODO remove!
-echo "PWD: $(pwd)"  # TODO remove!
-echo "LS: $(ls)"  # TODO remove!
-echo "LOCAL_DIRECTORY: $LOCAL_DIRECTORY"  # TODO remove!
-SOURCE_DIRECTORY=$(realpath "$GITHUB_WORKSPACE/$LOCAL_DIRECTORY")  # TODO correct!
+SOURCE_DIRECTORY=$(realpath "$GITHUB_WORKSPACE/$LOCAL_DIRECTORY")  # TODO correct?
 echo "The source directory for deployment is $SOURCE_DIRECTORY"
+# TODO commiter from GitHub Pages?
+# TODO git config.
+if [ -n "$NAME" ]  # i.e. test if not empty
+then
+  NAME="$GITHUB_ACTOR"  # TODO correct?
+fi
+if [ -n "$EMAIL" ]  # i.e. test if not empty
+then
+  EMAIL="TODO"  # TODO correct!
+fi
+echo "Configuring git committer to be $NAME <$EMAIL>"
 exit 1  # TODO correct!
 
 # Prepare
 echo "# Prepare"
-# TODO complete!
+exit 1  # TODO correct!
 
 # Deploy
 echo "# Deploy"
-# TODO complete!
+exit 1  # TODO correct!

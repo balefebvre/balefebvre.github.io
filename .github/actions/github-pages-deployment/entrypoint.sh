6a4ee28ffb772552dda5cee8e001b880da38a4a7
balefebvre <baptiste.lefebvre@ens.fr>
2020-04-14 01:43:23 +0200

    Test

--- .github/actions/github-pages-deployment/entrypoint.sh
+++ .github/actions/github-pages-deployment/entrypoint.sh
@@ -25,6 +25,7 @@ echo "TOKEN: $TOKEN"
 echo "LOCAL_DIRECTORY: $LOCAL_DIRECTORY"
 echo "NAME: $NAME"
 echo "EMAIL: $EMAIL"
+echo "TEMPORARY_DIRECTORY: $TEMPORARY_DIRECTORY"
 
 # Miscellaneous
 echo "PWD: $(pwd)"  # TODO remove!
@@ -77,48 +78,48 @@ fi
 echo "Using temporary work directory $WORK_DIRECTORY"
 cd "$WORK_DIRECTORY"
 
-# Deploy
-echo "# Deploy"
-# TODO git clone.
-# TODO git init.
-echo "Initializing local git repository."
-git init .
-# TODO copy files.
-echo "Copying $SOURCE_DIRECTORY contents to $WORK_DIRECTORY."
-rsync -rl --exclude ".git" --delete "$SOURCE_DIRECTORY/" .
-# TODO ...
-# TODO git config.
-echo "Configuring git committer to be $NAME <$EMAIL>."
-git config user.name "$NAME"
-git config user.email "$EMAIL"
-# TODO git commit.
-echo "Preparing to deploy $TARGET_BRANCH branch to GitHub Pages"
-# # TODO git commit hook!
-# # TODO deployment file!
-# # TODO cname!
-git add -A .
-ALLOW_EMPTY_COMMIT=""  # TODO correct!
-if [ -z "$ALLOW_EMPTY_COMMIT" ]  # i.e. test if empty
-then
-  GIT_COMMIT_OPTS=""
-else
-  GIT_COMMIT_OPTS="--allow-empty"
-fi
-COMMIT_MESSAGE="Deploy $PROJECT_NAME to $URL:$TARGET_BRANCH"  # TODO correct!
-if [ -n "$COMMIT_MESSAGE" ]  # i.e. test if not empty
-then
-  GIT_COMMIT_MSG_OPTS="-m \"$COMMIT_MESSAGE\""
-else
-  exit 1  # TODO correct!
-fi
-git commit $GIT_COMMIT_OPTS -q $GIT_COMMIT_MSG_OPTS
-# TODO git push.
-echo "Pushing to $URL."
-# git push $GIT_PUSH_OPTS --quiet "$REMOTE_URL" "$TARGET_BRANCH":"$TARGET_BRANCH" > /dev/null 2>&1
-# TODO git status.
-VERBOSE=""
-if [ -n "$VERBOSE" ]  # i.e. test if not empty
-  git status
-else
-  git status  # TODO remove?
-fi
+## Deploy
+#echo "# Deploy"
+## TODO git clone.
+## TODO git init.
+#echo "Initializing local git repository."
+#git init .
+## TODO copy files.
+#echo "Copying $SOURCE_DIRECTORY contents to $WORK_DIRECTORY."
+#rsync -rl --exclude ".git" --delete "$SOURCE_DIRECTORY/" .
+## TODO ...
+## TODO git config.
+#echo "Configuring git committer to be $NAME <$EMAIL>."
+#git config user.name "$NAME"
+#git config user.email "$EMAIL"
+## TODO git commit.
+#echo "Preparing to deploy $TARGET_BRANCH branch to GitHub Pages"
+## # TODO git commit hook!
+## # TODO deployment file!
+## # TODO cname!
+#git add -A .
+#ALLOW_EMPTY_COMMIT=""  # TODO correct!
+#if [ -z "$ALLOW_EMPTY_COMMIT" ]  # i.e. test if empty
+#then
+#  GIT_COMMIT_OPTS=""
+#else
+#  GIT_COMMIT_OPTS="--allow-empty"
+#fi
+#COMMIT_MESSAGE="Deploy $PROJECT_NAME to $URL:$TARGET_BRANCH"  # TODO correct!
+#if [ -n "$COMMIT_MESSAGE" ]  # i.e. test if not empty
+#then
+#  GIT_COMMIT_MSG_OPTS="-m \"$COMMIT_MESSAGE\""
+#else
+#  exit 1  # TODO correct!
+#fi
+#git commit $GIT_COMMIT_OPTS -q $GIT_COMMIT_MSG_OPTS
+## TODO git push.
+#echo "Pushing to $URL."
+## git push $GIT_PUSH_OPTS --quiet "$REMOTE_URL" "$TARGET_BRANCH":"$TARGET_BRANCH" > /dev/null 2>&1
+## TODO git status.
+#VERBOSE=""
+#if [ -n "$VERBOSE" ]  # i.e. test if not empty
+#  git status
+#else
+#  git status  # TODO remove?
+#fi
